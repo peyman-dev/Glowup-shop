e9bfac5416c5b6d65eacc2e1d4161096d3ba6eec
Peyman Ahmadi <peymangeek@gmail.com>
2025-11-18 20:34:20 +0330

    Links are redirecting to the Payment App

--- app/_components/templates/product/bottom-bar/index.tsx
+++ app/_components/templates/product/bottom-bar/index.tsx
@@ -26,7 +26,7 @@ const BottomBar = () => {
         </div>
         <Link
           tabIndex={1}
-          href={"PAYMENT_URL"}
+          href="reymit.ir/Jazabiyatclub"
           className="max-w-max px-5 text-sm h-12 border-2 border-red-800 hover:opacity-90 flex items-center justify-center gap-2 rounded-md duration-300 transition-all bg-linear-to-t from-red-600 to-red-500 text-white font-medium focus-within:ring-4 ring-red-500/40"
         >
           خرید دوره
